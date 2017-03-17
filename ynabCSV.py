0baf26fe990c33af785fcea2fcd2e4d99d739212
Adrian Nilsson <adnilsson@users.noreply.github.com>
2017-03-17 12:03:38 +0100

    Removed qualified import from locale, and  #of csv output-loop iterations  is no longer hardcoded.
    I want to clearly show that I use locale.atof and not the depricated
    string.atof, hence I removed the qualified import. Also include info on
    how YNAB handles categories, for future reference.

--- ynabCSV.py
+++ ynabCSV.py
@@ -2,12 +2,18 @@ from datetime import datetime
 from collections import namedtuple
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-from locale import *
+import locale
 import csv
 
+csvHeader = [['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow']]
 
-
-#csvRows = [['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow']]
+# ------- On YnabEntry -------
+# Categories will only import if the category already exists in your budget 
+# file with the exact same name. Otherwise the categories will be ignored 
+# when importing the file. Also, make sure that the categories are listed 
+# with the master category, followed by a colon, then the sub category.  
+# For example: 
+# Everyday Expenses: Groceries
 YnabEntry = namedtuple('YnabEntry', 'date payee category memo outflow inflow')
 BankEntry = namedtuple('BankEntry', 'date transaction memo amount balance')
 
@@ -15,29 +21,38 @@ BankEntry = namedtuple('BankEntry', 'date transaction memo amount balance')
 # 2. create a dict mapping common companies in the 'trasaction' field to ynab payees
 # 3. User dialog window asking what the payee should be when no payee was found. and add to dict
 # 4. another dict for payees to categories?? (test how well ynab handles empty category-fields first)
-# 5. Let user hardcode transactions thah should be ignored.
+# 5. Let user hardcode transactions that should be ignored.
 
+def namedtupleLen(tupleArg):
+    return len(tupleArg._fields)
+
+# For some reason, I can't get locale.atoi to behave, so I use this instead.
 def strToInt(floatString):
-    return int(atof(floatString))
+    return int(locale.atof(floatString))  
 
 # arg should be a BankEntry
 def parseRow(bankline):
     # For my bank's csv-files I need ',' for decimal separator
     # and '.' for thousand separator
-    setlocale(LC_NUMERIC, 'Italian_Italy.1252')
+    loc = locale.getlocale()
+    locale.setlocale(locale.LC_NUMERIC, 'Italian_Italy.1252') 
 
     assert type(bankline) is BankEntry, 'the given argument is not a line from the bank\'s csv-file'
-    assert strToInt(bankline.amount) != 0, 'A transaction must be of more than 1 unit in the currency used'
+    assert (not bankline.amount)  == False, 'A transaction must have an amount'
+
+    amount = locale.atof(bankline.amount)
+    assert abs(amount) > 0.00, 'A transaction must be of more than 0.00 units in the currency used'
     
-    bankInflow  = abs(atof(bankline.amount)) if strToInt(bankline.amount) > 0 else ''
-    bankOutflow = abs(atof(bankline.amount)) if strToInt(bankline.amount) < 0 else ''
+    bankInflow  = abs(amount) if amount > 0 else ''
+    bankOutflow = abs(amount) if amount < 0 else ''
 
     #entry_date = datetime.strptime('2016-10-26', '%Y-%m-%d') #covert to date
     date = datetime.strptime(bankline.date, '%Y-%m-%d') #covert to date
     dateStr = date.strftime('%Y/%m/%d')    #get string in the desired format
 
-    #TODO: Reset locale
-    
+    # Restore previous locale
+    locale.setlocale(locale.LC_ALL, loc) 
+
     return YnabEntry(date=dateStr, payee= '', category='', memo=bankline.memo, outflow=bankOutflow, inflow = bankInflow) 
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
@@ -54,12 +69,12 @@ outputFile = open('ynabImport.csv', "w", encoding='utf-8', newline='')
 
 reader = csv.reader(inputFile)
 writer = csv.writer(outputFile)
-writer.writerow(['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow'])
+writer.writerow(csvHeader)
 
 for row in reader:
     if reader.line_num == 1:    # Skip first row
         continue
-    if (row and len(row) == 5):
+    if (row and len(row) == namedtupleLen(BankEntry)) :
         print(parseRow(BankEntry._make(row)))
         writer.writerow(parseRow(BankEntry._make(row)))
     #csvRows.append(parseRow(row))
