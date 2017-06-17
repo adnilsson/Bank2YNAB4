4256d15d4027b39e59aa943c9f83e05679179e7c
Adrian Nilsson <adnilsson@users.noreply.github.com>
2017-06-18 01:12:43 +0200

    refractoring and nicer print messages
    Separated parsing input from writing output. Also, we now print
    information about how successful the parsing was.

--- ynabCSV.py
+++ ynabCSV.py
@@ -23,8 +23,10 @@ BankEntry = namedtuple('BankEntry', 'date transaction memo amount balance')
 # 3. User dialog window asking what the payee should be when no payee was found. and add to dict
 # 5. Let user hardcode transactions that should be ignored.
 
+
 def namedtupleLen(tupleArg):
     return len(tupleArg._fields)
+
 
 # arg should be a BankEntry
 def parseRow(bankline):
@@ -33,11 +35,16 @@ def parseRow(bankline):
     loc = locale.getlocale()
     locale.setlocale(locale.LC_NUMERIC, 'Italian_Italy.1252') 
     
-    if type(bankline) is not BankEntry: raise TypeError('the given argument is not a line from the bank\'s csv-file')
-    if not bankline.amount: raise ValueError('A transaction must have an amount')
+    if type(bankline) is not BankEntry: 
+        raise TypeError('{0} is not a line from the bank\'s csv-file'
+                        .format(bankline))
+    if not bankline.amount: 
+        raise ValueError('A transaction must have an amount')
 
     amount = locale.atof(bankline.amount)
-    if abs(amount) < 0.01: raise ValueError('A transaction must be >= 0.01 units in the currency used')
+    if abs(amount) < 0.01: 
+        raise ValueError('A transaction must be >= 0.01' 
+                         'units in the currency used')
     
     bankInflow  = abs(amount) if amount > 0 else ''
     bankOutflow = abs(amount) if amount < 0 else ''
@@ -48,51 +55,83 @@ def parseRow(bankline):
     # Restore previous locale
     locale.setlocale(locale.LC_ALL, loc) 
 
-    return YnabEntry(date=dateStr, payee= '', category='', memo=bankline.memo, outflow=bankOutflow, inflow = bankInflow) 
+    return YnabEntry(date=dateStr, payee= '', category='', 
+                     memo=bankline.memo, outflow=bankOutflow, 
+                     inflow = bankInflow) 
+
+
+def parseRows():
+    parsedRows = [] 
+    emptyRows = 0
+
+    try:
+        inputPath = getFile()
+    except (IOError, NameError, OSError) as e:
+            print('Failed to locate input file: {0}'.format(e))  
+            return []  
+
+    with open(inputPath, encoding='utf-8', newline='')  as inputFile:
+        reader = csv.reader(inputFile)
+        try:
+            for row in reader:
+                if reader.line_num == 1: # Skip first row (header)
+                    continue
+                if (row and len(row) != namedtupleLen(BankEntry)):
+                    warnings.warn(badFormatWarn(row), RuntimeWarning) 
+                elif (row):
+                    try:
+                        validRow = parseRow(BankEntry._make(row))
+                        print(validRow) # Row successfully parsed
+                        parsedRows.append(validRow)
+                    except (ValueError, TypeError) as e:
+                        msg ='\n\t{0}\n\tError: {1}'.format(row,e)
+                        warnings.warn(badFormatWarn(msg), RuntimeWarning)
+                else:
+                    warnings.warn(
+                        '\n\tSkipping row {0}: {1}'
+                        .format(reader.line_num, row), RuntimeWarning
+                        )
+                    emptyRows += 1
+        except csv.Error as e:
+            sys.exit('file %s\n line %s: %s' % (inputFile, row, e))    
+        else: 
+            print('{0}/{1} line(s) successfully parsed '
+                  '(ignored {2} blank line(s)).'
+                  .format(len(parsedRows), reader.line_num-1, emptyRows))
+    return parsedRows
+
 
 def getFile():
     inputPath = askopenfilename(
                 filetypes=[('CSV files', '*.csv'),
-                           ('All files', '*'),
+                           ('All files', '*'), # Used for debugging only
                            ],
-                initialdir='.')
+                initialdir='.'
+                )
     if inputPath:
-        file = open(inputPath, encoding='utf-8', newline='')
-        return file
-    else: raise OSError('Invalid file path')   
+        return inputPath
+    else: 
+        raise OSError('Invalid file path')   
+
 
 def badFormatWarn(entry):
-    return 'Incorrectly formated row:\n\t{0}\n Skipping...'.format(entry)
+    return '\n\tIncorrectly formated row:{0}\n\t Skipping...'.format(entry)
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing        
 
-# Attempt to parse input file to a YNAB-formatted csv file 
-try:
-    with getFile() as inputFile:
-        with open('ynabImport.csv', "w", encoding='utf-8', newline='') as outputFile:
-            reader = csv.reader(inputFile)
-            writer = csv.writer(outputFile)
-            writer.writerow(csvHeader)
+Tk().withdraw() # keep the root window from appearing        
 
-            try:
-                for row in reader:
-                    if reader.line_num == 1:    # Skip first row
-                        continue
-                    if (row and len(row) != namedtupleLen(BankEntry)):
-                        warnings.warn(badFormatWarn(row), RuntimeWarning) 
-                    elif (row):
-                        try:
-                            parsedRow = parseRow(BankEntry._make(row))
-                            print(parsedRow)
-                            writer.writerow(parsedRow)
-                        except (ValueError, TypeError) as e:
-                            msg ='{0}\n\tError: {1}'.format(row,e)
-                            warnings.warn(badFormatWarn(msg), RuntimeWarning)
-            except csv.Error as e:
-                sys.exit('file %s, line %d: %s' % (inputFile, reader.line_num, e))
-except (IOError, NameError, OSError) as e:
-    print('Failed to load input file: {0}'.format(e))
-else:
-    print("Done")
+# Attempt to parse input file to a YNAB-formatted csv file
+with open('ynabImport.csv', 'w', encoding='utf-8', newline='') as outputFile:
+    writer = csv.writer(outputFile)
+    try:
+        parsedRows = parseRows()
+        writer.writerow(csvHeader)
+        writer.writerows(parsedRows)
+    except (ValueError, TypeError) as e:
+        msg ='{0}\n\tError: {1}'.format(row,e)
+        warnings.warn(badFormatWarn(msg), RuntimeWarning)
+    except csv.Error as e:
+        sys.exit('file %s, line %d: %s' % (outputFile, writer.line_num, e)) 
+
     
  
