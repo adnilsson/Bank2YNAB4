243c024c25869c283aaeeec14f1cd2d612719fda
Adrian Nilsson <adnilsson@users.noreply.github.com>
2018-06-03 19:25:12 +0200

    Introduce GUI for selecting bank
    bank2ynab.py is now the main file to execute

---
+++ bank2ynab.py
@@ -0,0 +1,65 @@
+from tkinter import Tk, StringVar
+from tkinter.ttk import Combobox, Frame, Label, Button
+from collections import namedtuple
+from ynabCSV import main
+
+
+#BANKNAMES = ('Nordea', 'ICA')
+Bank = namedtuple('Bank', ['name', 'header', 'delimiter'])
+banks = dict()
+
+def toKey(name : str) -> str:
+    key = [c for c in name if c.isalpha()]
+    key = ''.join(key)
+    return key.lower()
+
+
+NordeaHeader = ['Date', 'Transaction', 'Memo', 'Amount', 'Balance']
+Nordea = Bank('Nordea', NordeaHeader, delimiter=',')
+banks[toKey(Nordea.name)] = Nordea
+
+IcaHeader = ['Date', 'Payee', 'Transaction', 'Memo', 'Amount', 'Balance']
+Ica = Bank('ICA Banken', IcaHeader, delimiter=';')
+banks[toKey(Ica.name)] = Ica
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.grid(padx=12, pady=10)
+        self.createWidgets()
+
+
+    def createWidgets(self):
+        self.label = Label(self, text="Choose Your Bank:")
+        self.label.grid(column=0, row=0, sticky='W', ipadx=2)
+
+        banknames = self.getNames(banks)
+        self.bankName = StringVar()
+        self.bankName.set(banknames[0])
+
+        self.bankChosen = Combobox(self, width=12, textvariable=self.bankName)
+        self.bankChosen['values'] = banknames
+        self.bankChosen.grid(column=1, row=0)
+
+        self.confirm = Button(self, text="Ok", command=self.convert)
+        self.confirm.grid(column=0, row=1, columnspan=2, sticky='E', pady=5)
+
+
+    def convert(self):
+        main(banks[toKey(self.bankName.get())], self.master)
+        
+
+    def getNames(self, banks):
+        names = []
+        
+        for _, b in banks.items():
+            names.append(b.name)
+        
+        names.sort()
+        return tuple(names)
+
+
+app = Application()
+app.master.title("Bank2YNAB")
+app.mainloop()
--- ynabCSV.py
+++ ynabCSV.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from collections import namedtuple
-from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import locale
 import csv
@@ -9,15 +8,7 @@ import warnings
 #TODO:
 # 1. create a mapping from  companies in the 'trasaction' field to ynab payees
 # 2. User dialog window asking what the payee should be when no payee was found, and add to mapping
-# 3. Dynamically change banks (config file or dialog)
 
-
-# Specified by YNAB4
-YNABHeader = ['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow']
-
-# Bank Header. 
-NordeaHeader = ['Date', 'Transaction', 'Memo', 'Amount', 'Balance']
-IcaHeader = ['Date', 'Payee', 'Transaction', 'Memo', 'Amount', 'Balance']
 
 # ------- On YnabEntry -------
 # Categories will only import if the category already exists in your budget 
@@ -28,98 +19,127 @@ IcaHeader = ['Date', 'Payee', 'Transaction', 'Memo', 'Amount', 'Balance']
 # Everyday Expenses: Groceries
 # 
 # Payee and category are currently always set to emrty string
-YnabEntry   = namedtuple('YnabEntry', ' '.join(YNABHeader).lower())
 
-NordeaEntry = namedtuple('NordeaEntry', ' '.join(NordeaHeader).lower()) 
-IcaEntry    = namedtuple('IcaEntry', ' '.join(IcaHeader).lower())
 
-BankEntry = NordeaEntry    # Change to the namedtuple that represents your bank's csv header
-csvDelimiter = ','      # set the delimiter used when parsing the csv file
+class Converter:
+    def __init__(self, bankHeader, delimiter):
+        # Specified by YNAB4
+        self.YNABHeader = ['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow']
+        self.YnabEntry  = namedtuple('YnabEntry', ' '.join(self.YNABHeader).lower())
+
+        self.BankEntry = namedtuple('BankEntry', ' '.join(bankHeader).lower()) 
+        self.csvDelimiter = delimiter
+
+
+    def convert(self, inputPath, toIgnore=[]):
+        if not inputPath:
+            return
+
+        # Attempt to parse input file to a YNAB-formatted csv file
+        # May raise OSError 
+        bankData = self.readInput(inputPath, toIgnore)
+        parsed = self.parseRows(bankData)
+        self.writeOutput(parsed)
+
+
+    def readInput(self, inputPath, toIgnore):
+        readRows = []
+        #toIgnore = readIgnore()
+        emptyRows = 0
+        ignoredRows = []
+        
+        with open(inputPath, encoding='utf-8', newline='')  as inputFile:
+            reader = csv.reader(inputFile, delimiter=self.csvDelimiter)
+            try:
+                for row in reader:
+                    if reader.line_num == 1: # Skip first row (header)
+                        continue
+
+                    if (row and len(row) != namedtupleLen(self.BankEntry)):
+                        msg = f' expected row length {namedtupleLen(self.BankEntry)},' \
+                                f' but got {len(row)} ({row})'
+                        warnings.warn(badFormatWarn(msg), RuntimeWarning)
+                    elif row:
+                        bankRow = self.BankEntry._make(row)
+                        for i in toIgnore:
+                            if i not in bankRow.transaction:
+                                readRows.append(bankRow)
+                            else:
+                                ignoredRows.append(bankRow)
+                    else:
+                        warnings.warn(
+                            f'\n\tSkipping row {reader.line_num}: {row}', 
+                            RuntimeWarning)
+                        emptyRows += 1
+            except csv.Error as e:
+                raise OSError(f'file {inputFile}\n line {row}: {e}')    
+            else: 
+                print('{0}/{1} line(s) successfully read '
+                      '(ignored {2} blank line(s) and '
+                       '{3} transactions found in accignore).'
+                      .format(len(readRows), reader.line_num-1, emptyRows, len(ignoredRows)))
+        
+        return readRows
+
+
+    def parseRows(self, bankRows):
+        parsedRows = [] 
+        for row in bankRows:
+            try:
+                parsedRows.append(self.parseRow(row))
+            except (ValueError, TypeError) as e:
+                msg = f'\n\t{row}\n\tError: {e}'
+                warnings.warn(badFormatWarn(msg), RuntimeWarning)
+
+        print(f'{len(parsedRows)}/{len(bankRows)} line(s) successfully parsed ')
+
+        return parsedRows
+
+
+    def parseRow(self, bankline):
+        if type(bankline) is not self.BankEntry: 
+            raise TypeError(f'{bankline} is not a line from the bank\'s csv-file')
+        if not bankline.amount: 
+            raise ValueError('A transaction must have an amount')
+        if not bankline.date: 
+            raise ValueError('A transaction must have a date')
+
+        strAmount = bankline.amount.strip()
+        amountSign = '-' if strAmount[0] == '-' else '+'
+        
+        bankInflow  = strAmount if amountSign == '+' else ''
+        bankOutflow = strAmount[1::] if amountSign == '-' else ''
+
+        date = datetime.strptime(bankline.date, '%Y-%m-%d') #convert to date
+        dateStr = date.strftime('%Y/%m/%d')    #get string in the desired format
+
+        # payee is not a mandatory field; set only if it exists
+        try:
+            payee = bankline.payee
+        except Exception as e:
+            payee = ''
+
+        return self.YnabEntry(date=dateStr, payee=payee, category='', 
+                         memo=bankline.memo, outflow=bankOutflow, 
+                         inflow = bankInflow)
+
+
+    def writeOutput(self, parsedRows):
+        with open('ynabImport.csv', 'w', encoding='utf-8', newline='') as outputFile:
+            writer = csv.writer(outputFile)
+            try:
+                writer.writerow(self.YNABHeader)
+                writer.writerows(parsedRows)
+                print('YNAB csv-file successfully written.')
+            except (ValueError, TypeError) as e:
+                msg =f'{row}\n\tError: {e}'
+                warnings.warn(badFormatWarn(msg), RuntimeWarning)
+            except csv.Error as e:
+                raise OSError(f'File {outputFile}, line {writer.line_num}: {e}')
 
 
 def namedtupleLen(tupleArg):
     return len(tupleArg._fields)
-
-def parseRow(bankline: BankEntry) -> YnabEntry:
-
-    if type(bankline) is not BankEntry: 
-        raise TypeError(f'{bankline} is not a line from the bank\'s csv-file')
-    if not bankline.amount: 
-        raise ValueError('A transaction must have an amount')
-    if not bankline.date: 
-        raise ValueError('A transaction must have a date')
-
-    strAmount = bankline.amount.strip()
-    amountSign = '-' if strAmount[0] == '-' else '+'
-    
-    bankInflow  = strAmount if amountSign == '+' else ''
-    bankOutflow = strAmount[1::] if amountSign == '-' else ''
-
-    date = datetime.strptime(bankline.date, '%Y-%m-%d') #convert to date
-    dateStr = date.strftime('%Y/%m/%d')    #get string in the desired format
-
-    # payee is not a mandatory field; set only if it exists
-    try:
-        payee = bankline.payee
-    except Exception as e:
-        payee = ''
-
-    return YnabEntry(date=dateStr, payee=payee, category='', 
-                     memo=bankline.memo, outflow=bankOutflow, 
-                     inflow = bankInflow)
-
-
-def parseRows(bankRows: [BankEntry]) -> [YnabEntry]:
-    parsedRows = [] 
-    for row in bankRows:
-        try:
-            parsedRows.append(parseRow(row))
-        except (ValueError, TypeError) as e:
-            msg = f'\n\t{row}\n\tError: {e}'
-            warnings.warn(badFormatWarn(msg), RuntimeWarning)
-
-    print(f'{len(parsedRows)}/{len(bankRows)} line(s) successfully parsed ')
-
-    return parsedRows
-
-
-def readInput(inputPath):
-    readRows = []
-    toIgnore = readIgnore()
-    emptyRows = 0
-    ignoredRows = []
-
-    with open(inputPath, encoding='utf-8', newline='')  as inputFile:
-        reader = csv.reader(inputFile, delimiter=csvDelimiter)
-        try:
-            for row in reader:
-                if reader.line_num == 1: # Skip first row (header)
-                    continue
-
-                if (row and len(row) != namedtupleLen(BankEntry)):
-                    msg = f' expected row length {namedtupleLen(BankEntry)},' \
-                            f' but got {row}'
-                    warnings.warn(badFormatWarn(msg), RuntimeWarning)
-                elif row:
-                    bankRow = BankEntry._make(row)
-                    for i in toIgnore:
-                        if i not in bankRow.transaction:
-                            readRows.append(bankRow)
-                        else:
-                            ignoredRows.append(bankRow)
-                else:
-                    warnings.warn(
-                        f'\n\tSkipping row {reader.line_num}: {row}', 
-                        RuntimeWarning)
-                    emptyRows += 1
-        except csv.Error as e:
-            sys.exit(f'file {inputFile}\n line {row}: {e}')    
-        else: 
-            print('{0}/{1} line(s) successfully read '
-                  '(ignored {2} blank line(s) and '
-                   '{3} transactions found in accignore).'
-                  .format(len(readRows), reader.line_num-1, emptyRows, len(ignoredRows)))
-    return readRows
 
 
 def readIgnore():
@@ -128,19 +148,6 @@ def readIgnore():
         for account in ignored:
             accounts.append(account)
     return accounts
-
-
-def writeOutput(parsedRows):
-    with open('ynabImport.csv', 'w', encoding='utf-8', newline='') as outputFile:
-        writer = csv.writer(outputFile)
-        try:
-            writer.writerow(YNABHeader)
-            writer.writerows(parsedRows)
-        except (ValueError, TypeError) as e:
-            msg =f'{row}\n\tError: {e}'
-            warnings.warn(badFormatWarn(msg), RuntimeWarning)
-        except csv.Error as e:
-            sys.exit(f'File {outputFile}, line {writer.line_num}: {e}')
 
 
 def getFile():
@@ -152,24 +159,26 @@ def getFile():
     if inputPath:
         return inputPath
     else: 
-        raise OSError('Invalid file path: {inputPath}\n')   
+        raise OSError('Invalid file path.')   
 
 
 def badFormatWarn(entry):
     return f'\n\tIncorrectly formated row:{entry}\n\t Skipping...'
 
 
-if __name__ == "__main__":
+def main(bank, root):
+    Bank2Ynab = Converter(bank.header, bank.delimiter)
 
-    Tk().withdraw() # keep the root window from appearing        
+    # Check for accignore.txt and obtain a list ofignored accounts. 
+    # It's okay to not have it.
+    try:
+        ignoredAccounts = readIgnore()
+    except OSError:
+        ignoredAccounts = []
 
-    # Attempt to parse input file to a YNAB-formatted csv file
     try:
         inputPath = getFile()
-        bankData = readInput(inputPath)
-        parsed = parseRows(bankData)
-        writeOutput(parsed)
-        input("Press Return to exit...")
+        Bank2Ynab.convert(inputPath, ignoredAccounts)
+        root.destroy()
     except (IOError, NameError, OSError) as e:
-        print(f'Failed to locate input file: {e}')  
-        sys.exit()  
+        print(f'Error: {e}') 
