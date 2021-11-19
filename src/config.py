69c0f87c2b77357407bb280a66dac3ef3bb369a4
Adrian Nilsson <adnilsson@users.noreply.github.com>
2021-11-20 00:26:39 +0100

    Restructure how the config is loaded
    Now, `BankConfig.from_config` only extract the config strings (raising
    `KeyErrors` if required keys are missing) and `__init__` instead
    validates and initializes helper structures.

--- src/config.py
+++ src/config.py
@@ -3,8 +3,14 @@ from dataclasses import dataclass
 import warnings
 import tomli
 from pathlib import Path
-from typing import Any, Callable, NamedTuple
+from typing import Any, Callable
 
+
+def _assert_not_empty(column_name: str) -> str:
+    if column_name == '':
+        raise ValueError('Empty column name')
+
+    return column_name
 
 class TransactionFormat(enum.Flag):
     """ Format for a CSV transaction column
@@ -36,29 +42,31 @@ class TransactionColumn():
         return cls(header_key=header_key, transaction_format=TransactionFormat.INFLOW)
 
     @classmethod
-    def from_dict(cls, ynab_mapping: dict[str, str | list[str]]) -> list["TransactionColumn"]:
+    def from_config(cls, outflows: str | list[str], inflows: str | list[str]) -> list["TransactionColumn"]:
         def to_list(val: str | list[str]) -> list[str]:
             """ Type check the value
             """
             is_str = lambda v: isinstance(v, str)
             if is_str(val):
-                return [val]
-            elif not isinstance(val, list):
+                return [_assert_not_empty(val)]
+
+            if not isinstance(val, list):
                 raise TypeError(f"Key '{val}' is {type(val)}, not a str or list")
             elif not all((is_str(v) for v in val)):
                 not_str = list(filter(lambda v: not is_str(v), val))
                 raise TypeError(f"Expected only list of str, but found {not_str} in list")
 
-            return val
+            return list(map(_assert_not_empty, val))
 
-        outflows = to_list(ynab_mapping['outflow'])
-        inflows = to_list(ynab_mapping['inflow'])
+        outflows_list = to_list(outflows)
+        inflows_list = to_list(inflows)
 
-        outflows_tc = {cls._outflow_column(oc) for oc in outflows}
-        inflows_tc = {cls._inflow_column(ic) for ic in inflows}
+        outflows_tc = {cls._outflow_column(oc) for oc in outflows_list}
+        inflows_tc = {cls._inflow_column(ic) for ic in inflows_list}
 
+        # Find AMOUNT columns
         transaction_columns = {tc.header_key: tc for tc in outflows_tc | inflows_tc}
-        amount_keys = set(outflows) & set(inflows)
+        amount_keys = set(outflows_list) & set(inflows_list)
         for k in amount_keys:
             transaction_columns[k] = cls(header_key=k, transaction_format=TransactionFormat.AMOUNT)
 
@@ -82,48 +90,63 @@ class BankConfig():
         self,
         name: str,
         date_format: str,
-        currency_format: CurrencyFormat,
+        thousands_separator: str,
+        decimal_point: str,
         date_column: str,
-        transaction_columns: (list[TransactionColumn] | None)=None,
+        outflow_columns: str | list[str],
+        inflow_columns: str | list[str],
         payee_column: (str | None)=None,
         memo_column: (str | None)=None,
         category_column: (str | None)=None,
         csv_delimiter: (str | None)=None,
         normalizer: (Callable[[str], str] | None)=None,
     ):
-        if date_column is None or date_column == '':
+        if name == '':
+            raise ValueError(f"The name column name is empty; {name=}")
+        self.name = name
+
+        if date_column == '':
             raise ValueError(f"The date column name is empty; {date_column=}")
         self._date_column = date_column
 
-        self.name = name
+        if date_format == '':
+            raise ValueError(f"The date format string is empty; {date_format=}")
         self.date_format = date_format
+
         self.csv_delimiter = ',' if csv_delimiter is None else csv_delimiter
-        self.currency_format = currency_format
+        if len(self.csv_delimiter) != 1:
+            raise ValueError(f"The CSV delimiter must be a single character, not '{csv_delimiter}'")
+
+        self.currency_format = CurrencyFormat(
+            thousands_sep=thousands_separator,
+            decimal_point=decimal_point,
+        )
+        if self.csv_delimiter == self.currency_format.decimal_point == ',':
+            warnings.warn(
+                "Both the delimiter and decimal point characters are ',' - make "
+                "sure all transaction values are properly quoted", UserWarning)
+        elif csv_delimiter == self.currency_format.thousands_sep == ',':
+            warnings.warn(
+                "Both the delimiter and thousands separator characters are ',' - make "
+                "sure all transaction values are properly quoted", UserWarning)
+
 
         self._payee_column = payee_column
         self._memo_column = memo_column
         self._category_column = category_column
 
-        self._transaction_columns = []
-        if transaction_columns is not None:
-            self._transaction_columns = transaction_columns
+        self._transaction_columns = TransactionColumn.from_config(outflow_columns, inflow_columns)
 
         self.normalizer = lambda x: x
         if normalizer is not None:
-            self.normalizer = normalizer # string pre-processing
+            self.normalizer = normalizer # string pre-processing function
 
     @property
     def date_column(self):
-        if self._date_column is None:
-            return None
-
         return self.normalizer(self._date_column)
 
     @property
-    def transaction_columns(self) -> list[TransactionColumn] | None:
-        if self._transaction_columns is None:
-            return None
-
+    def transaction_columns(self) -> list[TransactionColumn]:
         normalized=[]
         for tc in self._transaction_columns:
             ntc = TransactionColumn(
@@ -131,7 +154,6 @@ class BankConfig():
                 transaction_format=tc.transaction_format,
             )
             normalized.append(ntc)
-
         return normalized
 
     @property
@@ -166,29 +188,17 @@ class BankConfig():
         name = toml_config['name']
 
         currency_config = toml_config['currency_format']
-        currency_format = CurrencyFormat(
-            thousands_sep=currency_config['thousands_separator'],
-            decimal_point=currency_config['decimal_point'],
-        )
+        thousands_separator = currency_config['thousands_separator']
+        decimal_point = currency_config['decimal_point']
 
         csv_config: dict[str, Any] = toml_config['csv']
         date_format = csv_config['date_format']
         csv_delimiter = csv_config.get('delimiter', ',')
-        if len(csv_delimiter) != 1:
-            raise ValueError(f"The CSV delimiter must be a single character, not '{csv_delimiter}'")
-
-        if csv_delimiter == currency_format.decimal_point == ',':
-            warnings.warn(
-                "Both the delimiter and decimal point characters are ',' - make "
-                "sure all transaction values are properly quoted", UserWarning)
-        elif csv_delimiter == currency_format.thousands_sep == ',':
-            warnings.warn(
-                "Both the delimiter and thousands separator characters are ',' - make "
-                "sure all transaction values are properly quoted", UserWarning)
 
         ynab_mapping = toml_config['ynab_mapping']
         date_column = ynab_mapping['date']
-        transaction_columns = TransactionColumn.from_dict(ynab_mapping)
+        outflow_columns = ynab_mapping['outflow']
+        inflow_columns = ynab_mapping['inflow']
         payee_column = ynab_mapping.get('payee')
         memo_column = ynab_mapping.get('memo')
         category_column = ynab_mapping.get('category')
@@ -197,9 +207,11 @@ class BankConfig():
             name=name,
             date_format=date_format,
             csv_delimiter=csv_delimiter,
-            currency_format=currency_format,
+            thousands_separator=thousands_separator,
+            decimal_point=decimal_point,
             date_column=date_column,
-            transaction_columns=transaction_columns,
+            outflow_columns=outflow_columns,
+            inflow_columns=inflow_columns,
             payee_column=payee_column,
             memo_column=memo_column,
             category_column=category_column,
