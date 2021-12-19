4de38d5b17f6bff419302a9c3f130f876b8b2909
Adrian Nilsson <adnilsson@users.noreply.github.com>
2021-12-19 23:48:41 +0100

    Remove Revolut v1

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -23,18 +23,6 @@ def test_nordea_v2():
     result = bank2ynab(nordea_config, csv_path)
     assert expect == result
 
-def test_revolut_v1(tmpdir):
-    csv_path = load_test_example('revolut_v1.csv')
-    toml_path = load_bank_config('revolut_v1.toml')
-    revolut_config = BankConfig.from_file(toml_path)
-
-    out_dir = Path(tmpdir)
-    fixed_file = fix_revolut.quote_numbers(csv_path, out_dir=out_dir)
-
-    expect = (True, 0, 0, 6, 6)
-    result = bank2ynab(revolut_config, fixed_file)
-    assert expect == result
-
 def test_revolut_v2():
     csv_path = load_test_example('revolut_v2.csv')
     toml_path = load_bank_config('revolut_v2.toml')
